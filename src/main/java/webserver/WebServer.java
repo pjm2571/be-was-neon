d8ee0fb3ef038efba4b4d9b32134c3cf8d84d19e
Zoonmy <pjm2571@naver.com>
2024-03-11 17:13:06 +0900

    feat: concurrent 패키지로 스레드 구조 변경 - ExecutorService.newFixedThreadPool을 통해 크기가 100인 스레드 풀 생성 - executor.execute()를 통해 클라이언트 socket의 스레드 생성

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
+    private static int THREAD_POOL_SIZE = 100;
 
     public static void main(String args[]) throws Exception {
         int port = 0;
@@ -22,12 +25,18 @@ public class WebServer {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
+            // 접속할 수 있는 스레드의 개수를 100개로 제한하여 생성한다.
+            ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                executor.execute(new RequestHandler(connection));
             }
+
+            // 서버가 종료되면 executor을 shutdown
+            executor.shutdown();
         }
+
     }
 }
