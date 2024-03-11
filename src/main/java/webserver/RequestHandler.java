a461320591e41cbbb76911aa43abb60b0c0f9a2a
Zoonmy <pjm2571@naver.com>
2024-03-11 22:11:57 +0900

    feat: 정적인 index.html 응답, HTTP Request 출력 구현 - request Line이 /index.html 로 주어질 때 	- static/index.html을 매핑 	- static/img 파일을 매핑 	- static.css 파일들을 매핑 		- Content-Type 분리를 통해 해결 - logger.debug를 활용하여 각각의 메소드 라인마다 message를 붙여 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,14 +5,18 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
+import static webserver.CONTENT_TYPE.*;
+import static webserver.ROUTE.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.StringUtils;
 
-public class RequestHandler implements Runnable {
-    private static final String STATIC_ROUTE = "src/main/resources/static";
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
+public class RequestHandler implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String REQUEST = "request";
+    private static final String HEADER = "header";
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -29,37 +33,49 @@ public class RequestHandler implements Runnable {
 
             String requestLine = br.readLine();    // GET /index.html HTTP/1.1
 
-            if (requestLine == null) {
-                return;
-            }
+            if (requestLine == null) return;    // http request line이 null 이라면 스레드 종료
 
-            logger.debug("request : {}", requestLine);  // HTTP Request의 첫번째 줄은 GET or POST 메소드
+            loggerDebug(REQUEST, requestLine);  // request 타입에 대한 log.debug
 
-            String path = StringUtils.getFirstToken(requestLine);
+            String path = StringUtils.getPath(requestLine);
 
-            StringBuilder headers = new StringBuilder(br.readLine());
+            StringBuilder headers = new StringBuilder(br.readLine());   // String 재할당을 쓰지 않기 위해 StringBuilder 사용
             while (!headers.toString().isEmpty()) {
-                logger.debug("header : {}", headers);
-                headers.replace(0, headers.length(), "");
-                headers.append(br.readLine());
+                loggerDebug(HEADER, headers.toString());
+                headers.replace(0, headers.length(), "");     // StringBuilder의 모든 내용을 ""으로 초기화 -> new StringBuilder보다 낫다.
+                headers.append(br.readLine());  // 다음 값 매핑
             }
 
-            DataOutputStream dos = new DataOutputStream(out);
+            DataOutputStream dos = new DataOutputStream(out);   // 데이터 처리를 위한 DataOutputStream 생성
 
-            byte[] body = Files.readAllBytes(new File(STATIC_ROUTE + path).toPath()); // path 설정
+            byte[] body = Files.readAllBytes(new File(STATIC.getRoute(path)).toPath()); // static 값만 매핑
+            // 추후에, template이 나오면 template으로 변경 가능성 있음
 
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            response200Header(dos, body.length, path);  // output header 작성
+
+            responseBody(dos, body);    // output body 작성
 
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+
+    /*
+     * requestMessage : request or header 임을 알려주는 메시지
+     * requestLine : httpRequestLine
+     * -> logger.debug 메소드를 활용해서 입력받은 requestLine을 디버그해준다.
+     */
+    private void loggerDebug(String requestMessage, String requestLine) {
+        logger.debug(requestMessage + " : {}", requestLine);
+    }
+
+
+    /* http response 200 : 성공적으로 http request가 수행되었다는 메세지*/
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String path) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes(getContentType(StringUtils.getType(path)));  // path 경로에서 .ivo, .svg, .html 등 확장자 명을 가져와서 content-type을 설정한다.
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
