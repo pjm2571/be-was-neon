0d410fab5c00c8dd5b2daebba416218a1d457abd
Zoonmy <pjm2571@naver.com>
2024-03-11 21:46:00 +0900

    feat: StringUtils 클래스를 검증하는 테스트 코드 구현 - 올바른 request line 입력 시, path 추출 - 올바르지 않은 request line 입력 시, 예외 처리
    - 메소드 확장자 타입 체크

---
+++ src/test/java/utils/StringUtilsTest.java
@@ -0,0 +1,51 @@
+package utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import webserver.CONTENT_TYPE;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class StringUtilsTest {
+
+    private final static String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    @DisplayName("request Line에서 PATH를 가져오는 테스트")
+    void get_path_at_requestLine() {
+        String httpRequestLine = "GET /img/ci_chevron-right.svg HTTP/1.1";
+
+        String expectedPath = "/img/ci_chevron-right.svg";
+
+        assertThat(StringUtils.getPath(httpRequestLine)).isEqualTo(expectedPath);
+    }
+
+    @Test
+    @DisplayName("올바르지 않은 형식의 Request가 들어갈 경우 테스트")
+    void get_path_at_invalid_requestLine() {
+        String invalidHttpRequestLine = "GET /index.html";
+
+        assertThatThrownBy(() -> StringUtils.getPath(invalidHttpRequestLine))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ERROR_MESSAGE);
+    }
+
+    @Test
+    @DisplayName("request의 path에서 확장자 타입 가져오는 테스트")
+    void get_type_at_requestPath() {
+        String requestPath = "/img/ci_chevron-right.svg";
+
+        assertThat(StringUtils.getType(requestPath)).isEqualTo(CONTENT_TYPE.svg.name());
+    }
+
+    @Test
+    @DisplayName("request path에서 확장자 타입 불일치 테스트")
+    void get_svg_type_error(){
+        String requestPath = "/img/ci_chevron-right.svg";
+
+        assertThat(StringUtils.getType(requestPath)).isNotEqualTo(CONTENT_TYPE.html.name());
+    }
+
+
+
+}
