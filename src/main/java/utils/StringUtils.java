093432df6ff257acb3aade8104d30e9e2a2804ce
Zoonmy <pjm2571@naver.com>
2024-03-12 11:40:45 +0900

    feat: Parm Line이 주어지면 =을 기준으로 오른쪽 값만 가져오는 기능 구현

--- src/main/java/utils/StringUtils.java
+++ src/main/java/utils/StringUtils.java
@@ -9,6 +9,9 @@ public class StringUtils {
     private static final String REQUEST_ERROR = ERROR + " 올바르지 않은 형식의 Request 입니다.";
     public static final String SPACE = " ";
     public static final String DOT = "\\.";
+    public static final String QUESTION = "\\?";
+    public static final String AND = "&";
+    public static final String EQUAL = "=";
 
     public static String getPath(String inputLine) {
         String[] tokens = inputLine.split(SPACE);
@@ -20,19 +23,23 @@ public class StringUtils {
 
     //create?userId=z&name=a&password=b&email=pj%40nave.com
     public static String getURL(String inputLine) {
-        String[] tokens = inputLine.split("\\?");   // 물음표를 기준으로 앞의 문자열만 가져온다.
+        String[] tokens = inputLine.split(QUESTION);   // 물음표를 기준으로 앞의 문자열만 가져온다.
         return tokens[0];
     }
 
     public static String[] getParams(String inputLine) {
-        String paramLine = inputLine.split("\\?")[1]; // 물음표를 기준으로 뒤의 문자열만 가져온다.
-
-        return paramLine.split("&");
+        String paramLine = inputLine.split(QUESTION)[1]; // 물음표를 기준으로 뒤의 문자열만 가져온다.
+        return paramLine.split(AND);
     }
 
 
     public static String getType(String inputLine) {
         String[] tokens = inputLine.split(DOT);
+        return tokens[1];
+    }
+
+    public static String getParamValue(String inputParamLine) {
+        String[] tokens = inputParamLine.split(EQUAL);
         return tokens[1];
     }
 }
