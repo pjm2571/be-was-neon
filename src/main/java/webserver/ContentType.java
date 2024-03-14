8409c2b127073c791e8515ef15657995581b37b5
Zoonmy <pjm2571@naver.com>
2024-03-14 16:02:30 +0900

    feat: js, jpg, jpeg, png 타입의 content-type 구현

--- src/main/java/webserver/ContentType.java
+++ src/main/java/webserver/ContentType.java
@@ -3,8 +3,12 @@ package webserver;
 public enum ContentType {
     html("Content-Type: text/html;charset=utf-8\r\n"),
     css("Content-Type: text/css;charset=utf-8\r\n"),
+    js("Content-Type: application/javascript\r\n"),
     svg("Content-Type: image/svg+xml\r\n"),
-    ico("Content-Type: image/x-icon\r\n");
+    ico("Content-Type: image/x-icon\r\n"),
+    png("Content-Type: image/png\r\n"),
+    jpg("Content-Type: image/jpeg\r\n"),
+    jpeg("Content-Type: image/jpeg\r\n");
     private final String type;
 
     ContentType(String type) {
