60e43079fe999b6836232de95f9dd6994bf25ec0
Zoonmy <pjm2571@naver.com>
2024-03-20 11:12:30 +0900

    feat: QueryUtill Test 구현 - GET 요청의 QueryLine 추출 테스트 - queryLine의 query key-value 추출 테스트

--- src/test/java/utils/QueryUtilsTest.java
+++ src/test/java/utils/QueryUtilsTest.java
@@ -1,6 +1,7 @@
 package utils;
 
 
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import webserver.utils.QueryUtils;
@@ -10,17 +11,27 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.*;
 
 class QueryUtilsTest {
+
     @Test
-    @DisplayName("QueryUtils map 리턴값 테스트")
-    void queryTest(){
-        String requestTarget = "/createUser?userId=id&password=pass&name=name";
+    @DisplayName("get query request의 queryline 파싱 테스트")
+    void getQueryLineTest() {
+        String queryRequest = "/createUser?userId=id&password=pass&name=name&email=zoonmy%40naver.com";
+
+        assertThat(QueryUtils.getQueryLine(queryRequest)).isEqualTo("userId=id&password=pass&name=name&email=zoonmy%40naver.com");
+    }
+
+    @Test
+    @DisplayName("query문 쿼리 요소들 정상 파싱 테스트")
+    void getQueriesTest() {
+        SoftAssertions softly = new SoftAssertions();
+
+        String requestTarget = "/createUser?userId=id&password=pass&name=name&email=zoonmy%40naver.com";
 
         Map<String, String> queries = QueryUtils.getQueries(requestTarget);
 
-        assertThat(queries.get("userId")).isEqualTo("id");
-        assertThat(queries.get("password")).isEqualTo("pass");
-        assertThat(queries.get("name")).isEqualTo("name");
-
-
+        softly.assertThat(queries.get("userId")).isEqualTo("id");
+        softly.assertThat(queries.get("password")).isEqualTo("pass");
+        softly.assertThat(queries.get("name")).isEqualTo("name");
+        softly.assertThat(queries.get("email")).isEqualTo("zoonmy%40naver.com");
     }
 }
