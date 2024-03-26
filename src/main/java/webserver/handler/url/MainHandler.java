3da90fe11ebf92e9e4f7968520994dcf41066011
Zoonmy <pjm2571@naver.com>
2024-03-26 18:40:50 +0900

    feat: /main 으로 접속할 경우의 get handle 구현

--- src/main/java/webserver/handler/url/MainHandler.java
+++ src/main/java/webserver/handler/url/MainHandler.java
@@ -1,14 +1,29 @@
 package webserver.handler.url;
 
 import config.Config;
+import webserver.Url;
 import webserver.handler.UrlHandler;
+import webserver.handler.error.ErrorHandler;
+import webserver.handler.file.StaticFileHandler;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
+import webserver.utils.HttpRequestUtils;
 
 public class MainHandler implements UrlHandler {
 
     @Override
     public HttpResponse handleRequest(HttpRequest httpRequest, Config config) {
-        return null;
+        return switch (httpRequest.getMethod()) {
+            case GET -> handleGet(httpRequest, config);
+            case POST -> ErrorHandler.get404Response();
+        };
+    }
+
+    private HttpResponse handleGet(HttpRequest httpRequest, Config config) {
+        if (httpRequest.getRequestLine().equals(Url.MAIN.getUrlPath())) {   // url경로와 같다면 staticFileHandler!
+            StaticFileHandler staticFileHandler = new StaticFileHandler();
+            return staticFileHandler.handleRequest(HttpRequestUtils.convertToStaticFileRequest(httpRequest), config);
+        }
+        return ErrorHandler.get404Response();   // URL 이외의 요청은 404!
     }
 }
