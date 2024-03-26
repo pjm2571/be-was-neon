33569b0f29a3b9e2e61eecaa5d8329c89c63b741
Zoonmy <pjm2571@naver.com>
2024-03-26 18:46:59 +0900

    feat: /main 으로 접속할 때, 로그인되어있지 않은 경우 root url로 이동하도록 구현

--- src/main/java/webserver/handler/url/MainHandler.java
+++ src/main/java/webserver/handler/url/MainHandler.java
@@ -1,13 +1,17 @@
 package webserver.handler.url;
 
 import config.Config;
+import db.SessionStore;
+import webserver.StatusCode;
 import webserver.Url;
+import webserver.constants.Constants;
 import webserver.handler.UrlHandler;
 import webserver.handler.error.ErrorHandler;
 import webserver.handler.file.StaticFileHandler;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
 import webserver.utils.HttpRequestUtils;
+import webserver.utils.SidUtils;
 
 public class MainHandler implements UrlHandler {
 
@@ -20,10 +24,31 @@ public class MainHandler implements UrlHandler {
     }
 
     private HttpResponse handleGet(HttpRequest httpRequest, Config config) {
+        if (!isLoggedIn(httpRequest)) { // 로그인되어있지 않은 경우 접근한다면 root로 리다이렉트
+            String startLine = HttpRequestUtils.generateResponseStartLine(StatusCode.FOUND);
+            String header = HttpRequestUtils.generateRedirectResponseHeader(Constants.ROOT_URL);
+            return new HttpResponse(startLine, header);
+        }
         if (httpRequest.getRequestLine().equals(Url.MAIN.getUrlPath())) {   // url경로와 같다면 staticFileHandler!
             StaticFileHandler staticFileHandler = new StaticFileHandler();
             return staticFileHandler.handleRequest(HttpRequestUtils.convertToStaticFileRequest(httpRequest), config);
         }
         return ErrorHandler.get404Response();   // URL 이외의 요청은 404!
+    }
+
+    private boolean isLoggedIn(HttpRequest httpRequest) {
+        try {
+            String sid = getSid(httpRequest.getHeaderValue(Constants.COOKIE));
+            return SessionStore.sessionIdExists(sid);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private String getSid(String cookie) {
+        if (cookie == null) {
+            throw new IllegalArgumentException("[ERROR] 쿠키가 없습니다.");
+        }
+        return SidUtils.getCookieSid(cookie);
     }
 }
