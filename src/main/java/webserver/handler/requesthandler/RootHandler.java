de561b26e06e6c965188643ebee0c66d5b7f4bcd
pjm2571 <pjm2571@naver.com>
2024-03-28 21:33:04 +0900

    feat: 로그인 시, 유저 이름 표시 구현

--- src/main/java/webserver/handler/requesthandler/RootHandler.java
+++ src/main/java/webserver/handler/requesthandler/RootHandler.java
@@ -2,6 +2,7 @@ package webserver.handler.requesthandler;
 
 import config.Config;
 import db.SessionStore;
+import model.User;
 import webserver.handler.HttpRequestHandler;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
@@ -12,16 +13,6 @@ import webserver.utils.SidUtils;
 public class RootHandler implements HttpRequestHandler {
     private static final String COOKIE = "Cookie";
     private static final String DEFAULT_FILE = "index.html";
-
-    private static final String REPLACEMENT = "<ul class=\"header__menu\">\n" +
-            "    <li class=\"header__menu__item\">\n" +
-            "        <a class=\"btn btn_contained btn_size_s\" href=\"/article\">글쓰기</a>\n" +
-            "    </li>\n" +
-            "    <li class=\"header__menu__item\">\n" +
-            "        <button id=\"logout-btn\" class=\"btn btn_ghost btn_size_s\" onclick=\"window.location.href='/logout'\">로그아웃</button>\n" +
-            "    </li>\n" +
-            "</ul>";
-
     private static final String PATTERN = "<ul(?:\\s+class=\"[^\"]*\")?>[\\s\\S]*?</ul>";
 
     @Override
@@ -40,7 +31,9 @@ public class RootHandler implements HttpRequestHandler {
         httpRequest = HttpRequestUtils.convertToStaticFileRequest(httpRequest);
         if (isLoggedIn(httpRequest)) {
             DynamicFileHandler dynamicFileHandler = new DynamicFileHandler(httpRequest);
-            return dynamicFileHandler.handleRequest(REPLACEMENT, PATTERN);
+            String userName = getUserName(httpRequest);
+            String replacement = getReplacement(userName);
+            return dynamicFileHandler.handleRequest(replacement, PATTERN);
         }
         StaticFileHandler staticFileHandler = new StaticFileHandler();
         return staticFileHandler.handleRequest(httpRequest);
@@ -68,5 +61,25 @@ public class RootHandler implements HttpRequestHandler {
 
     private boolean isDefaultFile(HttpRequest httpRequest) {
         return httpRequest.getRequestLine().endsWith(DEFAULT_FILE);
+    }
+
+    private String getUserName(HttpRequest httpRequest) {
+        String cookie = httpRequest.getHeaderValue(COOKIE);
+        String sid = SidUtils.getCookieSid(cookie);
+        User user = SessionStore.getUserBySid(sid);
+        return user.getName();
+    }
+
+    private String getReplacement(String userName) {
+
+        return "<ul class=\"header__menu\">\n" +
+                "<li class=\"user-name\">" + userName + "</li>\n" +
+                "    <li class=\"header__menu__item\">\n" +
+                "        <a class=\"btn btn_contained btn_size_s\" href=\"/article\">글쓰기</a>\n" +
+                "    </li>\n" +
+                "    <li class=\"header__menu__item\">\n" +
+                "        <button id=\"logout-btn\" class=\"btn btn_ghost btn_size_s\" onclick=\"window.location.href='/logout'\">로그아웃</button>\n" +
+                "    </li>\n" +
+                "</ul>";
     }
 }
