d9f332a5030c281cd337a380d28d3cc431c04b0f
pjm2571 <pjm2571@naver.com>
2024-04-03 22:16:47 +0900

    feat: article content를 읽어오는 기능 구현 - 읽기만 하고, 저장은 구현하지 않음. 리팩토링 필요.

--- src/main/java/webserver/handler/requesthandler/ArticleHandler.java
+++ src/main/java/webserver/handler/requesthandler/ArticleHandler.java
@@ -8,9 +8,14 @@ import webserver.sid.SidValidator;
 import webserver.utils.HttpRequestUtils;
 import webserver.utils.HttpResponseUtils;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class ArticleHandler implements HttpRequestHandler {
     private static final String REDIRECT_URL = "/";
-
+    private static final String LINE_SEPARATOR = "\n";
 
     @Override
     public HttpResponse handleRequest(HttpRequest httpRequest) {
@@ -39,7 +44,57 @@ public class ArticleHandler implements HttpRequestHandler {
     }
 
     private HttpResponse handlePost(HttpRequest httpRequest) {
+        InputStreamReader reader = new InputStreamReader(new ByteArrayInputStream(httpRequest.getBody()));
+        String boundary = getBoundary(httpRequest.getHeaderValue("Content-Type"));
+
+        try {
+            String line = readLine(reader);
+            if (line.equals(boundary)) {
+                StringJoiner sj = new StringJoiner(LINE_SEPARATOR);
+                while (!(line = readLine(reader)).equals(boundary)) {
+                    if (!line.isEmpty()) {
+                        sj.add(line);
+                    }
+                }
+                System.out.println(sj.toString());
+            }
+
+            // 이제 시작
+            System.out.println(readLine(reader));
+            System.out.println(readLine(reader));
+            System.out.println(readLine(reader));
+
+            // body를 읽자!
+
+            System.out.println(readLine(reader));
+
+        } catch (IOException e) {
+            e.getMessage();
+            return HttpResponseUtils.get500Response();
+        }
+
         return HttpResponseUtils.get500Response();
+    }
+
+    private String getBoundary(String contentType) {
+        String boundarySubstr = "boundary=";
+        int boundaryIndex = contentType.indexOf(boundarySubstr);
+        String boundary = contentType.substring(boundaryIndex + boundarySubstr.length());
+        return "--" + boundary;
+    }
+
+    private String readLine(InputStreamReader reader) throws IOException {
+        StringBuilder line = new StringBuilder();
+        int character;
+        while ((character = reader.read()) != -1) {
+            if (character == '\n') {
+                break;
+            }
+            if (character != '\r') {
+                line.append((char) character);
+            }
+        }
+        return line.toString();
     }
 
 
